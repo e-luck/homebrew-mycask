ed26ed237c1a7db6e9065400d95fbee21d0f37e7
e-luck <eiji.fukushima@gmail.com>
2014-03-02 21:18:25 +0900

    first commit

---
+++ Casks/macvim-kaoriya.rb
@@ -0,0 +1,7 @@
+class MacvimKaoriya < Cask
+  url 'https://github.com/splhack/macvim/releases/download/20140107/macvim-kaoriya-20140107.dmg'
+  homepage 'https://code.google.com/p/macvim-kaoriya/'
+  version '20140107'
+  sha1 '9908b98f79c5bbaf77f9444958f2416faf6fbf1f'
+  link 'MacVim.app'
+end
