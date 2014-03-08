ffc36f2dccf4e48f7f69490c3859514ccf1ba99c
e-luck <eiji.fukushima@gmail.com>
2014-03-08 14:08:39 +0900

    add brew-mycask.rb

---
+++ brew-mycask.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class BrewMycask < Formula
+  homepage 'https://github.com/e-luck/homebrew-mycask/'
+  url 'https://github.com/e-luck/homebrew-mycask/.git'
+  def install
+  end
+end
